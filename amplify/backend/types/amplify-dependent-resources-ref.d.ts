a348a60c8a7613ee6da448f210b685fb88b79e2b
RedJ <ounbakhwa@gmail.com>
2023-05-17 14:16:12 +0200

    Added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3fc2e6f60": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
